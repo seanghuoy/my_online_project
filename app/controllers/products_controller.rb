6d1c4bc90e708b6c00c1de609611a9ff55f499a2
seanghuoy <sanseanghuoy143@gmail.com>
2015-08-05 16:33:39 +0700

    first commit my online project

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,23 @@
+class ProductsController < ApplicationController
+	def index
+
+		@products = Product.all
+	
+	end
+
+	def new
+		
+	end
+
+	def show
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+end
